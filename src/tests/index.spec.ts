355e7a7294eacbe54952b5f044df4c50818d94ca
William Vandergraaf <willtheorangeguy@users.noreply.github.com>
2023-11-11 17:35:59 -0700

    Update tests

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,7 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('meta is correct', async ({ page }) => {
-  await page.goto("http://localhost:4321/");
+test('page loads correctly', async ({ page }) => {
+  await page.goto('http://localhost:4321/');
+  const title = await page.title();
+  expect(title).toBe('DFD Style Guide');
+});
 
-  await expect(page).toHaveTitle('DFD Style Guide');
+test('theme is set correctly', async ({ page }) => {
+  await page.goto('http://localhost:4321/');
+  const theme = await page.evaluate(() => localStorage.getItem('theme'));
+  expect(theme).toBe('light');
 });
