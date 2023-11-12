4a634e8c2fecad2043fe110a781b91e53219d802
William Vandergraaf <willtheorangeguy@users.noreply.github.com>
2023-11-11 17:31:41 -0700

    Add sample tests

---
+++ src/tests/index.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('meta is correct', async ({ page }) => {
+  await page.goto("http://localhost:4321/");
+
+  await expect(page).toHaveTitle('DFD Style Guide');
+});
