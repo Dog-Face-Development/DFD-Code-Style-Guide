27482edbe819ab896512d367068489da460a6062
William Vandergraaf <willtheorangeguy@users.noreply.github.com>
2023-12-06 17:40:24 -0700

    Add playwright tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,13 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("page loads correctly", async ({ page }) => {
-  await page.goto("http://localhost:4321/");
+  await page.goto("/");
   const title = await page.title();
   expect(title).toBe("DFD Style Guide");
-});
-
-test("theme is set correctly", async ({ page }) => {
-  await page.goto("http://localhost:4321/");
-  const theme = await page.evaluate(() => localStorage.getItem("theme"));
-  expect(theme).toBe("light");
 });
